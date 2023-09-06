1621453a409b76cf7f69efc748ad519cd2699387
COR Coding <corcoding@DESKTOP-PH48BSF.localdomain>
2023-09-06 10:04:26 -0400

    added the required JS lines to utilize packages.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { login } = require('./models')
+var path = require('path')
+const bcrypt = require('bcrypt');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.urlencoded({ extended: false }))
+const winston = require('winston');
+
